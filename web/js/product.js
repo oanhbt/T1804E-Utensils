94d364fcb6c9b6aea97cc1b628d5b763f9d14504
kienbui <buiduckien1999@gmail.com>
2018-10-12 18:31:25 +0700

    procduct

--- web/js/product.js
+++ web/js/product.js
@@ -23,29 +23,102 @@ var product = [{
 			id:1,
 			name: "Japanese knife for chef blade 20.3cm stainless steel pressed abs (black)",
 			alias: 'dao_nau_bep_nhat',
-			img:'images/dao_nau_bep.jpg',
+			img:"images/dao_nau_bep.jpg",
+			img1:"images/dao_nhat.jpg",
+			img2:"images/dao_nhat2.jpg",
+		
 			price: "50",
 			details:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Blade for chef blade 20.3cm stainless steel laminated abs</font></font></h6>\
-						<p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Japanese knife 20.3cm with blade made of durable stainless steel, shiny. </font><font style="vertical-align: inherit;">This material is safe, does not contain toxic chemicals when exposed to food. </font><font style="vertical-align: inherit;">The product is designed for use in cutting vegetables and food. </font><font style="vertical-align: inherit;">The blade is only 3mm thick, for thin and fine slices.</font><font style="vertical-align: inherit;">The surface of the blade is smooth and easy to handle, so easy to clean after use. </font><font style="vertical-align: inherit;">The ABS handle does not slip, so it is safe for the user. </font><font style="vertical-align: inherit;">Sturdy clamping, making it easy to operate in Japan</font></font></p>'
+						<p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Japanese knife 20.3cm with blade made of durable stainless steel, shiny. </font><font style="vertical-align: inherit;">This material is safe, does not contain toxic chemicals when exposed to food. </font><font style="vertical-align: inherit;">The product is designed for use in cutting vegetables and food. </font><font style="vertical-align: inherit;">The blade is only 3mm thick, for thin and fine slices.</font><font style="vertical-align: inherit;">The surface of the blade is smooth and easy to handle, so easy to clean after use. </font><font style="vertical-align: inherit;">The ABS handle does not slip, so it is safe for the user. </font><font style="vertical-align: inherit;">Sturdy clamping, making it easy to operate in Japan</font></font></p>',
+		  
+		    informationid:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Japanese knife for chef blade 20.3cm stainless steel laminated ABS(Black)</font></font></h6>\
+						<p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">\
+						Japanese chef cooker 20.3cm with blade made of durable stainless steel, shiny. This material is safe, does not contain toxic chemicals when exposed to food. </font><font style="vertical-align: inherit;">\
+						Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie</font><font style="vertical-align: inherit;">\
+						blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt </font><font style="vertical-align: inherit;">\
+						The blade is only 3mm thick, for thin and fine slices.</font><font style="vertical-align: inherit;">\
+						ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore </font><font style="vertical-align: inherit;">\
+						magna aliqua.</font></font></p>'
+					
+
 		},{
 			id:2,
 			name: "Kitchen knife Mitsuboshi Dao-10 170mm thick 2mm laminated Japanese wood",
 			alias: 'dao_nau_bep_Kitchen_knife_Mitsuboshi',
 			img:'images/Kitchen_knife_Mitsuboshi.jpg',
-			price: "50",
-			details:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Kitchen knife Mitsuboshi Dao-10 170mm thick 2mm thick Japanese wood laminate is made of stainless steel 2mm thick, super durable, long blunt. Handles made of natural wood.</font></font></h6>'
-					
+			img1:'images/dao_bep.jpg',
+			img2:'images/dao_10.jpg',
+			
+			price: "100",
+			details:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Kitchen knife Mitsuboshi Dao-10 170mm thick 2mm thick Japanese wood laminate is made of stainless steel 2mm thick, super durable, long blunt. Handles made of natural wood.</font></font></h6>',
+			 informationid:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Made in Japan</font></font></h6>\
+						<p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">\
+						Dimensions The blade length is 115mm; 2mm thick </font><font style="vertical-align: inherit;">\
+						Material Stainless steel, ABS plastic</font><font style="vertical-align: inherit;">\
+						KH QS-X</font><font style="vertical-align: inherit;">\
+						Japanese 11.5cm fruit knife has a blade made of stainless steel, beautiful shiny. This material is safe, does not contain toxic chemicals when exposed to food.</font><font style="vertical-align: inherit;">\
+						</font></font></p>'			
  
 		},{
 			id:3,
-			name:""
+			name: "Cat knife 50cm",
+			alias: 'dao_meo_50cm',
+			img:'images/dao_meo.jpg',
+			img1:'images/dao_meo1.jpg',
+			img2:'images/dao_meo2.jpg',
+		
+			price: "150",
+			details:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">rolling:wood,material:tweezers automotive,the blade: 32cm </font></font></h6>',
+			 informationid:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Brand No Brand</font></font></h6>\
+						<p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">\
+						Cat knife line </font><font style="vertical-align: inherit;">\
+						SKU 245993978_VNAMZ-315777088</font><font style="vertical-align: inherit;">\
+						KH QS-X</font><font style="vertical-align: inherit;">\
+						Warranty Type With Purchase Invoice,Warranty 1 month</font><font style="vertical-align: inherit;">\
+						</font></font></p>'			
+ 
+		},{
+			id:4,
+			name: "Knife 8 stainless steel wooden soles Goodlife MK80",
+			alias: 'dao_Knife_8',
+			img:'images/bo_dao_8.jpg',
+			img1:'images/bo_dao_81.jpg',
+			img2:'images/bo_dao_82.jpg',
+		
+			price: "200",
+			details:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">rolling:wood,material:tweezers automotive,the blade: 32cm </font></font></h6>',
+			 informationid:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Brand No Brand</font></font></h6>\
+						<p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">\
+						Cat knife line </font><font style="vertical-align: inherit;">\
+						SKU 245993978_VNAMZ-315777088</font><font style="vertical-align: inherit;">\
+						KH QS-X</font><font style="vertical-align: inherit;">\
+						Warranty Type With Purchase Invoice,Warranty 1 month</font><font style="vertical-align: inherit;">\
+						</font></font></p>'			
+ 
+		},{
+			id:5,
+			name: "3 bottom Fivestar 26cm non-stick cookware using magnetic stove",
+			alias: 'chao_chong_dinh_3_day',
+			img:'images/chao.jpg',
+			img1:'images/chao1.jpg',
+			img2:'images/chao2.jpg',
+		
+			price: "250",
+			details:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">rolling:wood,material:tweezers automotive,the blade: 32cm </font></font></h6>',
+			 informationid:'<h6><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Brand No Brand</font></font></h6>\
+						<p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">\
+						Cat knife line </font><font style="vertical-align: inherit;">\
+						SKU 245993978_VNAMZ-315777088</font><font style="vertical-align: inherit;">\
+						KH QS-X</font><font style="vertical-align: inherit;">\
+						Warranty Type With Purchase Invoice,Warranty 1 month</font><font style="vertical-align: inherit;">\
+						</font></font></p>'			
+ 
 		}
 	];
 $(document).ready(function() {
 	var path_name_url = window.location.href; // Desktop/test_json.html#sp1
-  	var alias = path_name_url.substring(path_name_url.indexOf("#sp") + 1, path_name_url.length);
   	var alias = path_name_url.substring(path_name_url.indexOf("#dao_nau_bep") + 1, path_name_url.length);
-  
+  var alias = path_name_url.substring(path_name_url.indexOf("#dao") + 1, path_name_url.length);
   	if (path_name_url.indexOf("draft.html") > 0) {
   		$.each(product, function(index, val) {
 		    $('#list_pr').append('<div class="col-md-3">'+
@@ -71,6 +144,34 @@ $(document).ready(function() {
 			'</div>');
 		});
   	}
+
+
+  	if  (path_name_url.indexOf("single.html") > 0) {
+  		$.each(product, function(index, val) {
+		    $('#data-thumb').append('<div class="flexslider1">'+
+				'<ul class="slides">'+
+					'<li data-thumb="'+product[index].img+'">'+
+					'<div class="thumb-image">'+
+					'<img src="'+product[index].img+'" data-imagezoom="true" class="img-fluid" alt=" ">'+
+					'	</div>'+
+					'	</li>'+
+					'<li data-thumb="'+product[index].img1+'">'+
+					'<div class="thumb-image">'+
+					'<img src="'+product[index].img1+'" data-imagezoom="true" class="img-fluid" alt=" ">'+
+					'	</div>'+
+					'	</li>'+
+					'<li data-thumb="'+product[index].img2+'">'+
+					'<div class="thumb-image">'+
+					'<img src="'+product[index].img2+'" data-imagezoom="true" class="img-fluid" alt=" ">'+
+					'	</div>'+
+					'	</li>'+
+					'	</ul>'+
+					'<div class="clearfix"></div>'+						
+				
+			'</div>');
+		});
+
+}
   	
   	console.log(path_name_url.indexOf("single.html"));
   	if (path_name_url.indexOf("single.html") > 0) {
@@ -79,10 +180,12 @@ $(document).ready(function() {
   			console.log(alias);
   			if (product[index].alias === alias) {
 		  		$('#id').html(product[index].id);
+		  		
 		  		$('#name').html(product[index].name);
 		  		$('#alias').html(product[index].alias);
 		  		$('#price').html( '$' + product[index].price);
 		  		$('#details').html(product[index].details);
+		  		$('#informationid').html(product[index].informationid);
 		  	}
   		});
   	}
